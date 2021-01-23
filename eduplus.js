ea636d34b5bf3933948ece52960f52a1d55d6efc
c1tizen <timotejferenc@gmail.com>
2021-01-23 23:09:39 +0100

    Added plugin functionality (work in progress))

---
+++ eduplus.js
@@ -0,0 +1,6 @@
+var styles = "@import 'eduplus.css'"
+var styleSheet = document.createElement("style")
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+styleSheet.href = "eduplus.css"
+document.head.appendChild(styleSheet)
