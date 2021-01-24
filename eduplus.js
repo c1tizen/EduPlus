6869fa7eeb948c867b0c486f0335cd1d4d32d62d
c1tizen <65108066+c1tizen@users.noreply.github.com>
2021-01-24 23:13:17 +0100

    Delete eduplus.js

--- eduplus.js
+++
@@ -1,6 +0,0 @@
-var styles = "@import 'eduplus.css'"
-var styleSheet = document.createElement("style")
-styleSheet.type = "text/css"
-styleSheet.innerText = styles
-styleSheet.href = "eduplus.css"
-document.head.appendChild(styleSheet)
